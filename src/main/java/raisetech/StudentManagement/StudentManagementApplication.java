2c5ef255fb0fd6701257b4167f34834b88a81495
ikim-atirom <ikim-atirom@users.noreply.github.com>
2024-10-08 00:40:44 +0900

    動作確認（追加）

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -16,17 +16,25 @@ public class StudentManagementApplication {
 
 	@GetMapping("/hello")
 	public String hello() {
-		StringUtils.isEmpty("");
-		//　コンソールに、メソッド呼び出し成功したことを表示
-		System.out.println("hello() method was called");
-		return "Hello, World!";
+		String name = "";
+		if (StringUtils.isEmpty(name)) {
+			//　コンソールに、メソッド呼び出し成功したことを表示
+			System.out.println("hello() method was called");
+			return "入力してください";
+		} else {
+			return "Hello," + name + "!";
+		}
 	}
 
 	@GetMapping("/test.apache.commons.lang")
 	public String test() {
-		StringUtils.isNumeric("12345");
-		//　コンソールに、メソッド呼び出し成功したことを表示
-		System.out.println("test() method was called");
-		return "数値です";
+		String number = "12345";
+		if (StringUtils.isNumeric(number)) {
+			//　コンソールに、メソッド呼び出し成功したことを表示
+			System.out.println("test() method was called");
+			return number + "は数値です";
+		} else {
+			return number + "は数値ではありません";
+		}
 	}
 }
