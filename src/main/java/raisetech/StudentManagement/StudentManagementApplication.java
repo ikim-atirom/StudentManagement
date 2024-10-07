6f6f40782dc37c91e7d91a4f122c199548fa2f23
ikim-atirom <ikim-atirom@users.noreply.github.com>
2024-10-07 22:57:17 +0900

    動作確認（追加）

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,8 +1,10 @@
 package raisetech.StudentManagement;
 
+import java.util.Scanner;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,13 @@ public class StudentManagementApplication {
 
 	@GetMapping("/hello")
 	public String hello() {
+		StringUtils.isEmpty("");
 		return "Hello, World!";
+	}
+
+	@GetMapping("/test.apache.commons.lang")
+	public String test() {
+		StringUtils.isNumeric("12345");
+		return "数値です";
 	}
 }
