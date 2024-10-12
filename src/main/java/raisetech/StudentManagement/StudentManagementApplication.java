1d2855bbc7ea05cdc8aaad5bf434ac395a0c78a1
ikim-atirom <ikim-atirom@users.noreply.github.com>
2024-10-12 13:40:03 +0900

    学生情報の更新機能（追加）

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,40 +1,49 @@
 package raisetech.StudentManagement;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	//　Listの初期化
+	private List<Student> studentList = new ArrayList<>();
+	private Student student = new Student("Luffy", 16 ,"Pirate");
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		String name = "";
-		if (StringUtils.isEmpty(name)) {
-			//　コンソールに、メソッド呼び出し成功したことを表示
-			System.out.println("hello() method was called");
-			return "入力してください";
-		} else {
-			return "Hello," + name + "!";
-		}
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		return student.getName() + "　" + student.getAge() + "歳" + "　" + student.getJob();
 	}
 
-	@GetMapping("/test.apache.commons.lang")
-	public String test() {
-		String number = "12345";
-		if (StringUtils.isNumeric(number)) {
-			//　コンソールに、メソッド呼び出し成功したことを表示
-			System.out.println("test() method was called");
-			return number + "は数値です";
-		} else {
-			return number + "は数値ではありません";
-		}
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name, int age, String job) {
+		student.setName(name);
+		student.setAge(age);
+		student.setJob(job);
+	}
+
+	@PostMapping("/studentName")
+	public void updateStudentName(String name) {
+		student.setName(name);
+	}
+
+	@PostMapping("/studentAge")
+	public void updateStudentAge(int age) {
+		student.setAge(age);
+	}
+
+	@PostMapping("/studentJob")
+	public void updateStudentJob(String job) {
+		student.setJob(job);
 	}
 }
