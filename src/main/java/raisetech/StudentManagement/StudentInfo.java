db9f3026dabfa49ad7072f9780feab18d3736b89
ikim-atirom <ikim-atirom@users.noreply.github.com>
2024-10-20 00:37:50 +0900

    学生情報の検索・登録・更新・削除機能（追加）

--- src/main/java/raisetech/StudentManagement/StudentInfo.java
+++ src/main/java/raisetech/StudentManagement/StudentInfo.java
@@ -1,10 +1,16 @@
 package raisetech.StudentManagement;
 
-public class Student {
+public class StudentInfo {
 
-  private String name;
-  private int age;
-  private String job;
+  String name;
+  int age;
+  String job;
+
+  public StudentInfo(String name, int age, String job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+  }
 
   public String getName() {
     return name;
